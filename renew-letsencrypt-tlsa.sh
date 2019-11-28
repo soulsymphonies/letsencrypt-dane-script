a08545e1de5b9e17398bce8a4be654763563fccf
Robert Strasser <github@pc-tiptop.de>
2019-11-28 11:11:05 +0100

    check if DNS resolution for domains

--- renew-letsencrypt-tlsa.sh
+++ renew-letsencrypt-tlsa.sh
@@ -32,6 +32,12 @@ fi
 exec > >(tee -i ${consoleLog})
 exec 2>&1
 
+# get IPv4 and IPv6 address of local webserver interface
+localIPv4=$( ip address show dev $webserverInterface scope global | awk '/inet / {split($2,var,"/"); print var[1]}' )
+if [ $IPv6active == "yes" ] ; then
+	localIPv6=$( ip address show dev $webserverInterface scope global | awk '/inet6 / {split($2,var,"/"); print var[1]}' )
+fi
+
 # get options from command line
 while getopts ":d:e:m:w:" opt; do
   case $opt in
@@ -99,8 +105,26 @@ certificateChainFilename="chain.pem"
 fullChainFilename="fullchain.pem"
 letsencryptLogDir="/var/log/letsencrypt"
 NOW=$(date +%s)
+dnsError="no"
 genCertCommand="certbot certonly --agree-tos --standalone --non-interactive --email $email --csr $certPath/$domainCN/$csrFilename --cert-path $certPath/$domainCN/$certFilename --key-path $certPath/$domainCN/$privateKeyFilename --rsa-key-size 4096 --chain-path $certPath/$domainCN/$certificateChainFilename --fullchain-path $certPath/$domainCN/$fullChainFilename --logs-dir $letsencryptLogDir --quiet -d $domainCN"
 ###########################################
+
+### check if DNS resolution of domains points to this host/webserver
+domainIPv4=$( dig +noall +answer +nottlid +short $domainCN a )
+if [ $localIPv4 != $domainIPv4 ] ; then
+	echo "$domainCN does not resolve to local IPv4 address/webserver" 
+    echo "please adjust your DNS settings"
+	dnsError="yes"
+fi 
+
+if [ $IPv6active == "yes" ] ; then
+	domainIPv6=$( dig +noall +answer +nottlid +short $domainCN aaaa )
+	if [ $localIPv6 != $domainIPv6 ] ; then
+		echo "$domainCN does not resolve to local IPv6 address/webserver" 
+		echo "please adjust your DNS settings"
+	fi 
+	dnsError="yes"
+fi
 
 ### ADJUSTING genCertCommand for additional DNS subject alternative names
 if [ "$multipleDnsAlternativeNames" = true ] ; then
@@ -110,205 +134,225 @@ if [ "$multipleDnsAlternativeNames" = true ] ; then
 		let i=0
 		while (( ${#dnsAlternativeNames[@]} > i )); do
 			echo "${dnsAlternativeNames[i]}"
+			# check if DNS alternative name resolves properly to host/webserver IPs
+			domainIPv4=$( dig +noall +answer +nottlid +short ${dnsAlternativeNames[i] a )
+			
+			if [ $localIPv4 != $domainIPv4 ] ; then
+				echo "$domainCN does not resolve to local IPv4 address/webserver" 
+				echo "please adjust your DNS settings"
+				dnsError="yes"
+			fi 
+
+			if [ $IPv6active == "yes" ] ; then
+				domainIPv6=$( dig +noall +answer +nottlid +short ${dnsAlternativeNames[i] aaaa )
+				if [ $localIPv6 != $domainIPv6 ] ; then
+					echo "$domainCN does not resolve to local IPv6 address/webserver" 
+					echo "please adjust your DNS settings"
+				fi 
+				dnsError="yes"
+			fi	
+			# add DNS alternative name to command
 			genCertCommand="$genCertCommand -d ${dnsAlternativeNames[i++]}"
 		done
 	fi
 ##########################################
 
-### CHECKING DIRECTORIES and FILES ###
-# checking if directory exists, if not create it
-if [ ! -d "$certPath/$domainCN" ]; then
-  mkdir -p $certPath/$domainCN
-fi
-
-# checking if private key file exist, if not create it
-if [ ! -f $certPath/$domainCN/$privateKeyFilename ]; then
-	echo ""
-	echo "### Private Key Section ###"
-    echo "Note: $certPath/$domainCN/$privateKeyFilename not found!"
-    echo "Private Key is required for this script to work"
-    echo "Generating private key $certPath/$domainCN/$privateKeyFilename with RSA 4096"
-    openssl genrsa -out $certPath/$domainCN/$privateKeyFilename 4096
-fi
-
-# generating csr file with specified settings from openssl.conf
-echo -e "\n"
-echo "### CSR Section ###"
-echo "Generating CSR configuration file /etc/ssl/csr/$domainCN-csr.conf"
-
-# checking if directory exists, if not create it
-if [ ! -d "/etc/ssl/csr/" ]; then
-  echo "/etc/ssl/csr directory did not exist, creating it"
-  mkdir -p /etc/ssl/csr/
-fi
-   
-# checking if csr file already exists, if yes then delete it
-if [ -f $csrConfigFileName ]; then
-  rm -rf $csrConfigFileName
-fi
-   
-# writing openssl CSR configuration 
-echo "[req]" >> $csrConfigFileName
-echo "distinguished_name = req_distinguished_name" >> $csrConfigFileName
-echo "req_extensions = v3_req" >> $csrConfigFileName
-echo "prompt = no" >> $csrConfigFileName
-echo "[req_distinguished_name]" >> $csrConfigFileName
-echo "C = $COUNTRY" >> $csrConfigFileName
-echo "ST = $STATE" >> $csrConfigFileName
-echo "L = $CITY" >> $csrConfigFileName
-echo "O = $ORGANIZATION" >> $csrConfigFileName
-echo "OU = $DEPARTMENT" >> $csrConfigFileName
-echo "CN = $domainCN" >> $csrConfigFileName
-echo "[v3_req]" >> $csrConfigFileName
-echo "keyUsage = keyEncipherment, dataEncipherment" >> $csrConfigFileName
-echo "extendedKeyUsage = serverAuth" >> $csrConfigFileName
-echo "subjectAltName = @alt_names" >> $csrConfigFileName
-echo "[alt_names]" >> $csrConfigFileName
-echo "DNS.1 = $domainCN" >> $csrConfigFileName
-##########################################
-
-# add additional alternative subject DNS names to CSR if set
-if [ "$multipleDnsAlternativeNames" = true ] ; then
-	let j=0
-	# first dns entry is already present, start with DNS.2 and increment
-	dnsCount=2;
-	while (( ${#dnsAlternativeNames[@]} > j )); do
-		echo "DNS.$dnsCount = ${dnsAlternativeNames[j++]}" >> $csrConfigFileName
-		((dnsCount++))
-	done
-fi
-
-# finally create actual CSR file with openssl command
-openssl req -new -out $certPath/$domainCN/$csrFilename -key $certPath/$domainCN/$privateKeyFilename -config /etc/ssl/csr/$domainCN-csr.conf
-##########################################
-
-
-# check if certificate already exists, evaluate its validity
-if [ ! -f $certPath/$domainCN/$certFilename ]; then
-  # if certificate does not exist set validity to 0
-	CERT_VALIDITY=0
-else
-  # if certificate exists read validity end date
-	OPENSSL_ENDDATE="$(openssl x509 -enddate -noout -in $certPath/$domainCN/$certFilename)"
-	# cutting first 9 letters of openssl output to get clean end date
-	CERT_ENDDATE=${OPENSSL_ENDDATE:9}
-	# convert to seconds EPOCH
-	CERT_ENDDATE=$(date -d "$CERT_ENDDATE" +%s)
-	# computing validity days
-	CERT_VALIDITY=$((($CERT_ENDDATE - $NOW) / (24*3600)))
-fi
-###################################################
-
-### RENEWING CERTIFICATE ###
-# if valid less then 14 days renew
-# if certificate does not exist create new certificate
-if [ $CERT_VALIDITY -lt 14 ]
-then
-	### STOPPING WEBSERVER(S) ###
-	if [ "$nginx" = true ] ; then
-		echo "Stopping nginx"
-		systemctl stop nginx
-	fi
-	
-	if [ "$apache" = true ] ; then
-		echo "Stopping apache2"
-		systemctl stop apache2
-	fi
-  ###########################################
-
-  # make a backup of previous certificate files
-	mkdir $certPath/$domainCN/$NOW
-	if [ -f $certPath/$domainCN/$certificateChainFilename ]; then
-		mv $certPath/$domainCN/$certificateChainFilename $certPath/$domainCN/$NOW/$certificateChainFilename
-		echo "old chainfile moved to $certPath/$domainCN/$NOW/$certificateChainFilename"
+if [ $dnsError == "no" ] ; then
+	### CHECKING DIRECTORIES and FILES ###
+	# checking if directory exists, if not create it
+	if [ ! -d "$certPath/$domainCN" ]; then
+	  mkdir -p $certPath/$domainCN
 	fi
 
-	if [ -f $certPath/$domainCN/$fullChainFilename ]; then
-		mv $certPath/$domainCN/$fullChainFilename $certPath/$domainCN/$NOW/$fullChainFilename
-		echo "old fullchain file moved to $certPath/$domainCN/$NOW/$fullChainFilename"
+	# checking if private key file exist, if not create it
+	if [ ! -f $certPath/$domainCN/$privateKeyFilename ]; then
+		echo ""
+		echo "### Private Key Section ###"
+		echo "Note: $certPath/$domainCN/$privateKeyFilename not found!"
+		echo "Private Key is required for this script to work"
+		echo "Generating private key $certPath/$domainCN/$privateKeyFilename with RSA 4096"
+		openssl genrsa -out $certPath/$domainCN/$privateKeyFilename 4096
 	fi
-	
-	if [ -f $certPath/$domainCN/$certFilename ]; then
-		mv $certPath/$domainCN/$certFilename $certPath/$domainCN/$NOW/$certFilename
-		echo "old certificate moved to $certPath/$domainCN/$NOW/$certFilename"
+
+	# generating csr file with specified settings from openssl.conf
+	echo -e "\n"
+	echo "### CSR Section ###"
+	echo "Generating CSR configuration file /etc/ssl/csr/$domainCN-csr.conf"
+
+	# checking if directory exists, if not create it
+	if [ ! -d "/etc/ssl/csr/" ]; then
+	  echo "/etc/ssl/csr directory did not exist, creating it"
+	  mkdir -p /etc/ssl/csr/
 	fi
-  ############################################
- 
-  ### RENEW CERTIFICATE ###
-	# non-interactively renew certificates with specified private key and certificate request file
-	eval $genCertCommand
+	   
+	# checking if csr file already exists, if yes then delete it
+	if [ -f $csrConfigFileName ]; then
+	  rm -rf $csrConfigFileName
+	fi
+	   
+	# writing openssl CSR configuration 
+	echo "[req]" >> $csrConfigFileName
+	echo "distinguished_name = req_distinguished_name" >> $csrConfigFileName
+	echo "req_extensions = v3_req" >> $csrConfigFileName
+	echo "prompt = no" >> $csrConfigFileName
+	echo "[req_distinguished_name]" >> $csrConfigFileName
+	echo "C = $COUNTRY" >> $csrConfigFileName
+	echo "ST = $STATE" >> $csrConfigFileName
+	echo "L = $CITY" >> $csrConfigFileName
+	echo "O = $ORGANIZATION" >> $csrConfigFileName
+	echo "OU = $DEPARTMENT" >> $csrConfigFileName
+	echo "CN = $domainCN" >> $csrConfigFileName
+	echo "[v3_req]" >> $csrConfigFileName
+	echo "keyUsage = keyEncipherment, dataEncipherment" >> $csrConfigFileName
+	echo "extendedKeyUsage = serverAuth" >> $csrConfigFileName
+	echo "subjectAltName = @alt_names" >> $csrConfigFileName
+	echo "[alt_names]" >> $csrConfigFileName
+	echo "DNS.1 = $domainCN" >> $csrConfigFileName
+	##########################################
+
+	# add additional alternative subject DNS names to CSR if set
+	if [ "$multipleDnsAlternativeNames" = true ] ; then
+		let j=0
+		# first dns entry is already present, start with DNS.2 and increment
+		dnsCount=2;
+		while (( ${#dnsAlternativeNames[@]} > j )); do
+			echo "DNS.$dnsCount = ${dnsAlternativeNames[j++]}" >> $csrConfigFileName
+			((dnsCount++))
+		done
+	fi
+
+	# finally create actual CSR file with openssl command
+	openssl req -new -out $certPath/$domainCN/$csrFilename -key $certPath/$domainCN/$privateKeyFilename -config /etc/ssl/csr/$domainCN-csr.conf
+	##########################################
+
+
+	# check if certificate already exists, evaluate its validity
+	if [ ! -f $certPath/$domainCN/$certFilename ]; then
+	  # if certificate does not exist set validity to 0
+		CERT_VALIDITY=0
+	else
+	  # if certificate exists read validity end date
+		OPENSSL_ENDDATE="$(openssl x509 -enddate -noout -in $certPath/$domainCN/$certFilename)"
+		# cutting first 9 letters of openssl output to get clean end date
+		CERT_ENDDATE=${OPENSSL_ENDDATE:9}
+		# convert to seconds EPOCH
+		CERT_ENDDATE=$(date -d "$CERT_ENDDATE" +%s)
+		# computing validity days
+		CERT_VALIDITY=$((($CERT_ENDDATE - $NOW) / (24*3600)))
+	fi
+	###################################################
+
+	### RENEWING CERTIFICATE ###
+	# if valid less then 14 days renew
+	# if certificate does not exist create new certificate
+	if [ $CERT_VALIDITY -lt 14 ]
+	then
+		### STOPPING WEBSERVER(S) ###
+		if [ "$nginx" = true ] ; then
+			echo "Stopping nginx"
+			systemctl stop nginx
+		fi
+		
+		if [ "$apache" = true ] ; then
+			echo "Stopping apache2"
+			systemctl stop apache2
+		fi
+	  ###########################################
+
+	  # make a backup of previous certificate files
+		mkdir $certPath/$domainCN/$NOW
+		if [ -f $certPath/$domainCN/$certificateChainFilename ]; then
+			mv $certPath/$domainCN/$certificateChainFilename $certPath/$domainCN/$NOW/$certificateChainFilename
+			echo "old chainfile moved to $certPath/$domainCN/$NOW/$certificateChainFilename"
+		fi
+
+		if [ -f $certPath/$domainCN/$fullChainFilename ]; then
+			mv $certPath/$domainCN/$fullChainFilename $certPath/$domainCN/$NOW/$fullChainFilename
+			echo "old fullchain file moved to $certPath/$domainCN/$NOW/$fullChainFilename"
+		fi
+		
+		if [ -f $certPath/$domainCN/$certFilename ]; then
+			mv $certPath/$domainCN/$certFilename $certPath/$domainCN/$NOW/$certFilename
+			echo "old certificate moved to $certPath/$domainCN/$NOW/$certFilename"
+		fi
+	  ############################################
+	 
+	  ### RENEW CERTIFICATE ###
+		# non-interactively renew certificates with specified private key and certificate request file
+		eval $genCertCommand
+		############################################
+	  
+	  ### RESTARTING WEBSERVER(S) ###
+		if [ "$nginx" = true ] ; then
+			echo "Starting nginx"
+			systemctl start nginx
+		fi
+		if [ "$apache" = true ] ; then
+			echo "Starting apache2"
+			systemctl start apache2
+		fi
+		############################################
+		
+	  
+	  ### RELOAD CONFIGURATION to use new certificate ###
+	   if [ "$nginx" = true ] ; then
+			echo "Reloading config for nginx..."
+			systemctl reload nginx
+		fi
+		
+		if [ "$apache" = true ] ; then
+			echo "Reloading config for apache2..."
+			systemctl reload apache2
+		fi
+		
+		if [ "$email" = true ] ; then
+			echo "Reloading config for postfix and dovecot..."
+			systemctl reload postfix
+			systemctl reload dovecot
+		fi
+	  ############################################
+
+	  ### PRINT SUMMARY ###
+		echo ""
+		echo "### Summary Section ###"
+		echo "--------"
+		echo "Domain name: $domainCN"
+		echo "Certficate: $certPath/$domainCN/$certFilename"
+		echo "Private Key: $certPath/$domainCN/$privateKeyFilename"
+		echo "Chain File: $certPath/$domainCN/$certificateChainFilename"
+		echo "Full Chain File: $certPath/$domainCN/$fullChainFilename"
+		echo "Renewed, validity was $CERT_VALIDITY days"
+	  
+	else
+	  ### DO NOT RENEW ###
+
+	  ### PRINT SUMMARY ###
+		echo ""
+		echo "### Summary Section ###"
+		echo "--------"
+		echo "Domain name: $domainCN"
+		echo "Certficate: $certPath/$domainCN/$certFilename"
+		echo "Private Key: $certPath/$domainCN/$privateKeyFilename"
+		echo "Chain File: $certPath/$domainCN/$certificateChainFilename"
+		echo "Full Chain File: $certPath/$domainCN/$fullChainFilename"
+		echo "Nothing done, validity ok, $CERT_VALIDITY days remaining"
+	  ############################################
+	fi
+
+	### OUTPUT TLSA Record SPKI ###
+	echo ''
+	echo 'The TLSA DNS Record for the Certificate is:'
+	echo '--------------------------------------------'
+	printf '_443._tcp.%s. IN TLSA 3 1 1 %s\n' \
+		$domainCN \
+		$(openssl req -in $certPath/$domainCN/$csrFilename -noout -pubkey |
+			openssl pkey -pubin -outform DER |
+			openssl dgst -sha256 -binary |
+			hexdump -ve '/1 "%02x"')
 	############################################
-  
-  ### RESTARTING WEBSERVER(S) ###
-    if [ "$nginx" = true ] ; then
-		echo "Starting nginx"
-		systemctl start nginx
-	fi
-	if [ "$apache" = true ] ; then
-		echo "Starting apache2"
-		systemctl start apache2
-	fi
-	############################################
-	
-  
-  ### RELOAD CONFIGURATION to use new certificate ###
-   if [ "$nginx" = true ] ; then
-		echo "Reloading config for nginx..."
-		systemctl reload nginx
-	fi
-	
-	if [ "$apache" = true ] ; then
-		echo "Reloading config for apache2..."
-		systemctl reload apache2
-	fi
-	
-	if [ "$email" = true ] ; then
-		echo "Reloading config for postfix and dovecot..."
-		systemctl reload postfix
-	    systemctl reload dovecot
-	fi
-  ############################################
-
-  ### PRINT SUMMARY ###
-	echo ""
-	echo "### Summary Section ###"
-	echo "--------"
-	echo "Domain name: $domainCN"
-	echo "Certficate: $certPath/$domainCN/$certFilename"
-	echo "Private Key: $certPath/$domainCN/$privateKeyFilename"
-	echo "Chain File: $certPath/$domainCN/$certificateChainFilename"
-	echo "Full Chain File: $certPath/$domainCN/$fullChainFilename"
-	echo "Renewed, validity was $CERT_VALIDITY days"
-  
-else
-  ### DO NOT RENEW ###
-
-  ### PRINT SUMMARY ###
-	echo ""
-	echo "### Summary Section ###"
-	echo "--------"
-	echo "Domain name: $domainCN"
-	echo "Certficate: $certPath/$domainCN/$certFilename"
-	echo "Private Key: $certPath/$domainCN/$privateKeyFilename"
-	echo "Chain File: $certPath/$domainCN/$certificateChainFilename"
-	echo "Full Chain File: $certPath/$domainCN/$fullChainFilename"
-    echo "Nothing done, validity ok, $CERT_VALIDITY days remaining"
-  ############################################
 fi
 
-### OUTPUT TLSA Record SPKI ###
-echo ''
-echo 'The TLSA DNS Record for the Certificate is:'
-echo '--------------------------------------------'
-printf '_443._tcp.%s. IN TLSA 3 1 1 %s\n' \
-    $domainCN \
-    $(openssl req -in $certPath/$domainCN/$csrFilename -noout -pubkey |
-        openssl pkey -pubin -outform DER |
-        openssl dgst -sha256 -binary |
-        hexdump -ve '/1 "%02x"')
-############################################
-
-if [ $CERT_VALIDITY -lt 14 ]; then
-    ### send console via email ###
+# send an email if validity is less than 14 days or if a DNS resolution error occurred 
+if [ $CERT_VALIDITY -lt 14 ] || [ $dnsError == "true" ]; then
     mailx -a "From: "$host" Certificates <"$reportemail_from">" -s "Certificate Script | "$host $reportemail_to < $consoleLog
 fi
